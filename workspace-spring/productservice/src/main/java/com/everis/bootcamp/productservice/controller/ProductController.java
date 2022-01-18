f4a7672494d075a611d15cca2f46e53e68b70287
jairosanchezb94 <jairosanchezb5@gmail.com>
2022-01-18 08:20:16 +0100

    subida de proyecto java + microservicios

---
+++ workspace-spring/productservice/src/main/java/com/everis/bootcamp/productservice/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.everis.bootcamp.productservice.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class ProductController {
+	@Value("${server.port}")
+	private String port;
+	
+	@Value("${product.product-name}")
+	private String name;
+	
+	public ProductController() {
+		super();
+	}
+
+	@GetMapping("/product")
+	public String getPort() {
+		return "El puerto del micro es " + port;
+	}
+	
+	@GetMapping("/product-name")
+	public String getProductName() {
+		return "El nombre del producto es " + name;
+	}
+
+}
